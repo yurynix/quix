c29f6e6fecfdcff9c411b6bcf61bbbe2110d668f
Tamir Houri <tamirh@wix.com>
2021-10-10 12:26:39 +0300

    autocomplete: remove SqlBae antlr4 js

--- quix-frontend/client/src/lib/language-parsers/presto-grammar/index.ts
+++
@@ -1,9 +0,0 @@
-import { SqlBaseLexer } from './lang/presto/SqlBaseLexer';
-import { SqlBaseListener } from './lang/presto/SqlBaseListener';
-import { SqlBaseParser } from './lang/presto/SqlBaseParser';
-import { SqlBaseVisitor } from './lang/presto/SqlBaseVisitor';
-
-export { SqlBaseLexer };
-export { SqlBaseListener };
-export { SqlBaseParser };
-export { SqlBaseVisitor };
